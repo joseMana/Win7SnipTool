43521eeae74f3f7397531479e30c80eeddd27855
joseMana <josemanangan@gmail.com>
2021-10-25 18:30:12 +0800

    - improvemnts

--- PaintHelper/KeyCatch.cs
+++ PaintHelper/KeyCatch.cs
@@ -17,6 +17,8 @@ namespace PaintHelper
         public static char lastCharPressed { get; set; }
         public static bool shouldPause = false;
         public static int pauseTime = int.Parse(ConfigurationManager.AppSettings["pauseTime"]);
+        public static int pauseTimeTempHolder = 0;
+        public static bool initializedNewThread = false;
         public static void Start(Action quit)
         {
             var currentPosition = Cursor.Position;
@@ -39,14 +41,44 @@ namespace PaintHelper
                     {
                         if (shouldPause)
                         {
+                            Debug.WriteLine("Entering if (shouldPause)");
+
                             DateTime startTime = DateTime.Now;
+                            initializedNewThread = true;
                             while ((DateTime.Now - startTime).Seconds < pauseTime)
                             {
-
+                                Debug.WriteLine("still looping through the datetime.now -starttime");
+                                Debug.WriteLine($"pausetime value is {pauseTime}");
+                                Debug.WriteLine($"{(DateTime.Now - startTime).Seconds} < {pauseTime}");
+                                Thread.Sleep(500);
                             }
                             shouldPause = false;
+                            initializedNewThread = false;
+                            pauseTime = int.Parse(ConfigurationManager.AppSettings["pauseTime"]);
+                            Debug.WriteLine("Ending the Loop, set should = false, initializedNewThread = false, pauseTime = int.Parse(ConfigurationManager.AppSettings)");
                         }
                     }).Start();
+                }
+                else
+                {
+                    if (initializedNewThread)
+                    {
+                        Debug.WriteLine("Entering if (initializedNewThread)");
+                        Debug.WriteLine("Adding value to pausetime");
+                        pauseTimeTempHolder = pauseTimeTempHolder + 100;
+
+                        if(pauseTimeTempHolder == 1000)
+                        {
+                            pauseTime++;
+                            pauseTimeTempHolder = 0;
+                        }
+
+                        Debug.WriteLine($"new value of pausetime {pauseTime}");
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Did not enter if (initializedNewThread)");
+                    }
                 }
             };
 
