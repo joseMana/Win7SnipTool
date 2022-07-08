66b72b7533edf8b0f898a86f22c71a6401526472
Joseph Manangan <jmanangan@3cloudsolutions.com>
2022-07-08 19:43:06 +0800

    - Update for new machine

--- PaintHelper/KeyCatch_Constants.cs
+++ PaintHelper/KeyCatch_Constants.cs
@@ -17,19 +17,19 @@ namespace PaintHelper
 
         private static extern bool ShowWindow([In] IntPtr hWnd, [In] int nCmdShow);
         public static InputSimulator simulator = new InputSimulator();
-        public static Point linePosition = new Point { X = 355, Y = 59 };
-        public static Point boxPosition = new Point { X = 411, Y = 62 };
-        public static Point greenColorPosition = new Point { X = 841, Y = 57 };
-        public static Point redColorPosition = new Point { X = 778, Y = 61 };
-        public static Point yellowColorPosition = new Point { X = 818, Y = 63 };
-        public static Point blueColorPosition = new Point { X = 862, Y = 60 };
+        public static Point linePosition = new Point { X = 381, Y = 61 };
+        public static Point boxPosition = new Point { X = 442, Y = 66 };
+        public static Point greenColorPosition = new Point { X = 901, Y = 61 };
+        public static Point redColorPosition = new Point { X = 829, Y = 60 };
+        public static Point yellowColorPosition = new Point { X = 876, Y = 59 };
+        public static Point blueColorPosition = new Point { X = 919, Y = 60 };
 
-        public static Point lineThicknessOptionPosition = new Point { X = 594, Y = 70 };
-        public static Point lineThickness1Position = new Point { X = 616, Y = 138 };
-        public static Point lineThickness2Position = new Point { X = 612, Y = 175 };
-        public static Point lineThickness3Position = new Point { X = 625, Y = 214 };
-        public static Point lineThickness4Position = new Point { X = 629, Y = 258 };
-        public static Point textInputPosition = new Point { X = 270, Y = 69 };
+        public static Point lineThicknessOptionPosition = new Point { X = 634, Y = 73 };
+        public static Point lineThickness1Position = new Point { X = 658, Y = 137 };
+        public static Point lineThickness2Position = new Point { X = 656, Y = 176 };
+        public static Point lineThickness3Position = new Point { X = 661, Y = 211 };
+        public static Point lineThickness4Position = new Point { X = 661, Y = 256 };
+        public static Point textInputPosition = new Point { X = 290, Y = 70 };
 
         public static IntPtr handle = Process.GetCurrentProcess().MainWindowHandle;
     }
--- PaintHelper/Program.cs
+++ PaintHelper/Program.cs
@@ -6,50 +6,70 @@ using System.Diagnostics;
 using GregsStack.InputSimulatorStandard;
 using System.Drawing;
 using System.Threading;
+using System.Runtime.InteropServices;
+using GregsStack.InputSimulatorStandard.Native;
+using WindowScrape.Types;
 
 namespace PaintHelper
 {
     class Program
     {
         public static InputSimulator simulator = new InputSimulator();
-        static void Main(string[] args)
-        {
-            Console.WindowHeight = 1;
-            Console.WindowWidth = 1;
+        [DllImport("User32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindow([In] IntPtr hWnd, [In] int nCmdShow);
+		public static void Main(string[] args)
+		{
+			Console.WindowHeight = 1;
+			Console.WindowWidth = 1;
 
-            if (Process.GetProcesses().Where(x => x.ProcessName.Contains("mspaint")).Count() == 1)
-            {
-                //paint
-                ProcessStartInfo paintProcess = new ProcessStartInfo()
-                {
-                    FileName = "mspaint.exe",
-                    WindowStyle = ProcessWindowStyle.Minimized
-                };
-                Process.Start(paintProcess);
-            }
+			foreach (var process in Process.GetProcessesByName("mspaint"))
+			{
+				process.Kill();
+			}
 
-            #region Screenshot
-            Bitmap imageFromClipboard;
-            int trycounter = 0;
-            while (true)
-            {
-                Program.simulator.Keyboard.KeyPress(GregsStack.InputSimulatorStandard.Native.VirtualKeyCode.MBUTTON | GregsStack.InputSimulatorStandard.Native.VirtualKeyCode.DOWN);
-                Thread.Sleep(1000);
-                try
-                {
-                    imageFromClipboard = (Bitmap)Clipboard.GetImage(); break;
-                }
-                catch
-                {
-                    if (trycounter == 5)
-                        throw new Exception();
+			Thread.Sleep(1000);
 
-                    trycounter++;
-                    continue;
-                }
-            }
-            #endregion
-
-        }
-    }
+			if ((from x in Process.GetProcesses()
+				 where x.ProcessName.Contains("mspaint")
+				 select x).Count() == 0)
+			{
+				ProcessStartInfo paintProcess = new ProcessStartInfo
+				{
+					FileName = "mspaint.exe",
+					WindowStyle = ProcessWindowStyle.Minimized
+				};
+				Process.Start(paintProcess);
+			}
+			int trycounter = 0;
+			while (true)
+			{
+				simulator.Keyboard.KeyPress(VirtualKeyCode.SNAPSHOT);
+				Thread.Sleep(1000);
+				try
+				{
+					Bitmap imageFromClipboard = (Bitmap)Clipboard.GetImage();
+				}
+				catch
+				{
+					if (trycounter == 5)
+					{
+						throw new Exception();
+					}
+					trycounter++;
+					continue;
+				}
+				break;
+			}
+			ShowWindow(HwndObject.GetWindowByTitle("Untitled - Paint").Hwnd, 9);
+			simulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_V);
+			Thread.Sleep(150);
+			Point position = default(Point);
+			position.X = 334;
+			position.Y = 68;
+			Cursor.Position = position;
+			Thread.Sleep(150);
+			simulator.Mouse.LeftButtonClick();
+		}
+	}
 }
