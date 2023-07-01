cf92aeab7cbd3370eff2f2ab312929ae4b7907b2
Joseph Manangan <jmanangan@3cloudsolutions.com>
2023-07-02 03:03:30 +0800

    - refactored

--- PaintHelper/Program.cs
+++ PaintHelper/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Media;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ConsoleApp1
@@ -8,37 +11,110 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // set the target time for the MessageBox
-            TimeSpan targetTime = new TimeSpan(22, 55, 0); // 10:55pm
+            List<Reminder> reminders = new List<Reminder>
+            {
+                new Reminder()
+                    .SetYear(2023)
+                    .SetMonth(7)
+                    .SetDay(3)
+                    .SetTime(20, 30, 0)
+                    .SetMessage("IT WWORKS"),
 
-            // enter an infinite loop to check the time and execute the statement at the right time
+                new Reminder()
+                    .SetYear(2023)
+                    .SetMonth(7)
+                    .SetDay(7)
+                    .SetTime(22, 30, 0)
+                    .SetMessage("ASK MAMA ABOUT INSURANCE"),
+
+                new Reminder()
+                    .SetYear(2023)
+                    .SetMonth(7).SetDay(3)
+                    .SetTime(22, 30, 0)
+                    .SetMessage("REMEMBER TO GET A CAR WASH")
+            };
+
+            Reminders reminderManager = new Reminders(reminders);
+
+            // Enter a loop to continuously check the date and time
             while (true)
             {
-                // get the current date and time
-                DateTime now = DateTime.Now;
+                reminderManager.CheckReminders();
 
-                // check if the current time is after the target time for today, and before midnight
-                if (now.TimeOfDay >= targetTime && now.TimeOfDay < TimeSpan.FromHours(24))
+                Task.Delay(TimeSpan.FromMinutes(1)).Wait();
+            }
+        }
+    }
+
+    class Reminder
+    {
+        private DateTime dateTime;
+        public string Message { get; private set; }
+
+        public Reminder SetYear(int year)
+        {
+            dateTime = new DateTime(year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, dateTime.Second);
+            return this;
+        }
+
+        public Reminder SetMonth(int month)
+        {
+            dateTime = new DateTime(dateTime.Year, month, dateTime.Day, dateTime.Hour, dateTime.Minute, dateTime.Second);
+            return this;
+        }
+
+        public Reminder SetDay(int day)
+        {
+            dateTime = new DateTime(dateTime.Year, dateTime.Month, day, dateTime.Hour, dateTime.Minute, dateTime.Second);
+            return this;
+        }
+
+        public Reminder SetTime(int hour, int minute, int second)
+        {
+            dateTime = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, hour, minute, second);
+            return this;
+        }
+
+        public Reminder SetMessage(string message)
+        {
+            Message = message;
+            return this;
+        }
+
+        public DateTime GetDateTime()
+        {
+            return dateTime;
+        }
+    }
+
+    class Reminders
+    {
+        private List<Reminder> reminders;
+
+        public Reminders(List<Reminder> reminders)
+        {
+            this.reminders = reminders;
+        }
+
+        public void CheckReminders()
+        {
+            DateTime now = DateTime.Now;
+
+            foreach (Reminder reminder in reminders)
+            {
+                if (now >= reminder.GetDateTime())
                 {
-                    // display a MessageBox with the message
-                    MessageBox.Show("PREPARE FOR DSU!!!!");
-
-                    SystemSounds.Exclamation.Play();
-                    System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
-                    SystemSounds.Exclamation.Play();
-                    System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
-                    SystemSounds.Exclamation.Play();
-
-                    // wait for the next day
-                    DateTime tomorrow = now.AddDays(1).Date + targetTime;
-                    TimeSpan timeToWait = tomorrow - now;
-                    System.Threading.Thread.Sleep(timeToWait);
+                    PlayAlertSound();
+                    MessageBox.Show(reminder.Message);
                 }
-                else
-                {
-                    // wait for a minute before checking again
-                    System.Threading.Thread.Sleep(TimeSpan.FromMinutes(1));
-                }
+            }
+        }
+        private void PlayAlertSound()
+        {
+            for (int i = 0; i < 3; i++) // Play the alert sound three times
+            {
+                SystemSounds.Exclamation.Play();
+                Task.Delay(TimeSpan.FromSeconds(1)).Wait();
             }
         }
     }
